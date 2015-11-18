3a95725700ecf75b2de3ffb240733b1cfb3bc997
Topher Hallquist <topher@quixey.com>
2015-11-18 15:17:04 -0800

    Added key constants to script

--- script.py
+++ script.py
@@ -2,6 +2,10 @@ import pandas
 import io
 import urllib2
 
+STOCK_KEY = "S&P 500"
+BILL_KEY = "3-month T.Bill"
+BOND_KEY = "10-year T. Bond"
+
 url = "https://raw.githubusercontent.com/chris-hallquist/python-finance-demo/master/stocks_bills_bonds.csv"
 data = urllib2.urlopen(url).read()
 datafram = pandas.read_csv(io.BytesIO(data))
