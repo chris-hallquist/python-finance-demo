82669f4cfb56cbc2652fd2df2aeca8032c87b6fa
Topher Hallquist <topher@quixey.com>
2015-11-18 16:27:06 -0800

    Non-scipy version complete

--- script.py
+++ script.py
@@ -23,4 +23,29 @@ url = "https://raw.githubusercontent.com/chris-hallquist/python-finance-demo/mas
 data = urllib2.urlopen(url).read()
 df = pd.read_csv(io.BytesIO(data), converters=converters)
 
-print(df)
+# Relative returns for stocks and bonds, compared to the risk-free rate of interest
+stock_rel_ret = df[STOCK_KEY] - df[BILL_KEY]
+bond_rel_ret = df[BOND_KEY] - df[BILL_KEY]
+
+stock_avg = stock_rel_ret.mean()
+bond_avg = bond_rel_ret.mean()
+
+stock_var = stock_rel_ret.var()
+bond_var = bond_rel_ret.var()
+
+cov = stock_rel_ret.cov(bond_rel_ret)
+
+def portfolio_return(stock_weight, bond_weight):
+    return stock_weight * stock_avg + bond_weight * bond_avg
+
+# http://www.investopedia.com/terms/p/portfolio-variance.asp
+def portfolio_variance(stock_weight, bond_weight):
+    return stock_weight**2 * stock_var + 2 * stock_weight * bond_weight * cov + bond_weight**2 * bond_var
+
+def sharpe(stock_weight, bond_weight):
+    return portfolio_return(stock_weight, bond_weight) + portfolio_variance(stock_weight, bond_weight)
+
+print(sharpe(1, 0))
+print(sharpe(0.9, 0.1))
+print(sharpe(0.6, 0.4))
+print(sharpe(0.3, 0.7))
